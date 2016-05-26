58372ecd45d3a54ae19cdde5a28698a1b18bec5c
cheeseBucket <apbward@gmail.com>
2016-05-26 20:40:10 +1000

    set up p03. ignore temp files created by emacs

---
+++ p03.scala
@@ -0,0 +1,18 @@
+// P03 (*)
+// Find the Kth element of a list.
+// By convention, the first element in the list is element 0.
+// Example:
+
+// scala> nth(2, List(1, 1, 2, 3, 5, 8))
+// res0: Int = 2
+
+object problem3 {
+  def main(args: Array[String]): Unit = {
+    println("result of problem must be 2")
+    println("result: " + nth(2, List(1, 1, 2, 3, 5, 8)))
+  }
+
+  def nth(num: Int, List[Int]): Int = {
+    
+  }
+}
