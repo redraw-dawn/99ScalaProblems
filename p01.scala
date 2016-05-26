16ce20e1f8f7b479ef75d460b116403b7c2852fc
Andrew Ward <apbward@gmail.com>
2016-05-26 17:59:18 +1000

    solved problem 1

---
+++ p01.scala
@@ -0,0 +1,21 @@
+// Find the last element of a list.
+//   Example:
+
+// scala> last(List(1, 1, 2, 3, 5, 8))
+//     res0: Int = 8
+
+object problem1 {
+
+  def main(args: Array[String]): Unit = {
+    println("result of problem must be 8")
+    println("result: " + last(List(1, 1, 2, 3, 5, 8)))
+  }
+
+  def last[A](ls: List[A]): A = {
+    ls match {
+      case x :: Nil => x
+      case x :: xs => last(xs)      
+      case Nil => throw(new Throwable)
+    }
+  }
+}
