75017fbd9f111adcc48e74c548ef4324373b9243
cheeseBucket <apbward@gmail.com>
2016-08-24 22:54:22 +1000

    p09 solved

--- p09.scala
+++ p09.scala
@@ -15,15 +15,34 @@ object problem9 {
     def pack[A](ls: List[A]): List[List[A]] = {
       def loop(ls: List[A], buildingList: List[A], accum: List[List[A]]): List[List[A]] = {
         ls match {
-          case Nil => accum
-          case x :: Nil => 
-          case a :: b :: bs =>
-            if (a == b) 
-              }
-        // maybe instead of PM use a function which counts how many there are of the same in a row an i.e.
-        // takeWhile coul be used?
+          case Nil =>
+            if (buildingList.isEmpty)
+              accum
+            else
+              buildingList :: accum
+          case x :: xs =>
+            buildingList.headOption match {
+              case Some(y) if x == y =>
+                loop(ls.tail, x :: buildingList, accum)
+              case _ =>
+                if (buildingList.isEmpty)
+                  loop(ls.tail, x :: Nil, accum)
+                else
+                  loop(ls.tail, x :: Nil, buildingList :: accum)
+            }
+        }
       }
-      loop(ls, Nil, Nil)
+      reverse(loop(ls, Nil, Nil))
+    }
+
+    def reverse[A](ls: List[A]): List[A] = {
+      def loop[A](ls: List[A], accum: List[A]): List[A] = {
+        ls match {
+          case Nil => accum
+          case x :: xs => loop(xs, x :: accum)
+        }
+      }
+      loop(ls, Nil)
     }
   }
 }
