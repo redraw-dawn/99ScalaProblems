857b1d02c06966454c6e867305b88106f1e50cb7
cheeseBucket <apbward@gmail.com>
2016-08-24 23:05:24 +1000

    begun p10

---
+++ p10.scala
@@ -0,0 +1,45 @@
+// P10 (*) Run-length encoding of a list.
+//   Use the result of problem P09 to implement the so-called run-length encoding data compression method. Consecutive duplicates of elements are encoded as tuples (N, E) where N is the number of duplicates of the element E.
+// Example:
+// scala> encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+//     res0: List[(Int, Symbol)] = List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
+
+object problem10 {
+  def main(args: Array[String]): Unit = {
+
+  }
+
+  def pack[A](ls: List[A]): List[List[A]] = {
+    def loop(ls: List[A], buildingList: List[A], accum: List[List[A]]): List[List[A]] = {
+      ls match {
+        case Nil =>
+          if (buildingList.isEmpty)
+            accum
+          else
+            buildingList :: accum
+        case x :: xs =>
+          buildingList.headOption match {
+            case Some(y) if x == y =>
+              loop(ls.tail, x :: buildingList, accum)
+            case _ =>
+              if (buildingList.isEmpty)
+                loop(ls.tail, x :: Nil, accum)
+              else
+                loop(ls.tail, x :: Nil, buildingList :: accum)
+          }
+      }
+    }
+    reverse(loop(ls, Nil, Nil))
+  }
+
+  def reverse[A](ls: List[A]): List[A] = {
+    def loop[A](ls: List[A], accum: List[A]): List[A] = {
+      ls match {
+        case Nil => accum
+        case x :: xs => loop(xs, x :: accum)
+      }
+    }
+    loop(ls, Nil)
+  }
+}
+}
